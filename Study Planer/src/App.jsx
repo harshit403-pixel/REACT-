5bb22cbd107f7c64b35917412f6ddb67c7eada21
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-03-28 23:53:13 +0530

    addecd

---
+++ Study Planer/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Study from './Study'
+const App = () => {
+  return (
+    <div style={{height:"100vh", width:"100vw" , overflowX:"hidden"}}>
+     <Study />
+    </div>
+  )
+}
+
+export default App
